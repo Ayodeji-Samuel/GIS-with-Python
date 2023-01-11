4443bc539c0010112f40f92dd5f5d5e4c327c07c
Ayodeji Samuel Makinde <makinde6@gmail.com>
2023-01-11 13:24:38 +0100

    Add files via upload
    Python code for converting multiple.tif images from a different folder into time series by selecting specific pixels Make the first column in the dataframe the date from the image files without the .tif extension, and the second column the Pixcel value of the specific point selected. Then save it as CSV

---
+++ chirps_to_tsv3.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import os
+import numpy as np
+import pandas as pd
+
+# specify the folder that contains the TIF images
+image_folder = r'C:\Users\23480\Documents\Paper\Anna Grant\extracted files'
+image_files = [f for f in os.listdir(image_folder) if f.endswith('.tif')]
+
+# specify the x,y pixel coordinates of the pixel that you want to extract
+x_coord = 10
+y_coord = 20
+
+data = []
+for file in image_files:
+    # Open the TIF file
+    img = Image.open(os.path.join(image_folder, file))
+    # Get the pixel value
+    pixel = img.getpixel((x_coord, y_coord))
+    # Extract date from file name without the .tif extension
+    date = os.path.splitext(file)[0]
+    # Append the data to a list
+    data.append([date, pixel])
+
+# Create the dataframe
+df = pd.DataFrame(data, columns=['date', 'pixel_value'])
+
+# Save the dataframe to a CSV file
+df.to_csv('image_timeseries.csv', index=False)
